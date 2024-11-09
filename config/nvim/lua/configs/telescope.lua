0b7ce71618a3205f2b1e98b641aedd9a85488f2f
airp <wenmin.pei@gmail.com>
2024-11-10 00:09:53 +0800

    solve

--- config/nvim/lua/configs/telescope.lua
+++ config/nvim/lua/configs/telescope.lua
@@ -1,21 +1,18 @@
-local options = {
-  defaults = {
-    mappings = {
-      i = {
-        -- ["<Tab>"] = require("telescope.actions").move_selection_next,
-        -- ["<S-Tab>"] = require("telescope.actions").move_selection_previous,
-        -- ["<C-j>"] = require("telescope.actions").toggle_selection + require("telescope.actions").move_selection_worse,
-        -- ["<C-k>"] = require("telescope.actions").toggle_selection + require("telescope.actions").move_selection_better,
-        ["<C-j>"] = require("telescope.actions").move_selection_next,
-        ["<C-k>"] = require("telescope.actions").move_selection_previous,
-        ["<Esc>"] = require("telescope.actions").close,
-      },
-    },
-    file_ignore_patterns = {
-      "%.pb%.go$",
-      "%.pb%.validate%.go$",
-    },
-  },
-}
+local options = function(_, conf)
+  conf.defaults.mappings.i = {
+    -- ["<Tab>"] = require("telescope.actions").move_selection_next,
+    -- ["<S-Tab>"] = require("telescope.actions").move_selection_previous,
+    -- ["<C-j>"] = require("telescope.actions").toggle_selection + require("telescope.actions").move_selection_worse,
+    -- ["<C-k>"] = require("telescope.actions").toggle_selection + require("telescope.actions").move_selection_better,
+    ["<C-j>"] = require("telescope.actions").move_selection_next,
+    ["<C-k>"] = require("telescope.actions").move_selection_previous,
+    ["<Esc>"] = require("telescope.actions").close,
+  }
+  conf.defaults.file_ignore_patterns = {
+    "%.pb%.go$",
+    "%.pb%.validate%.go$",
+  }
+  return conf
+end
 
 return options
