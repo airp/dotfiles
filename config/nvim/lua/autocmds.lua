79cdc24251264388a27319479b9b1777cfd9cec3
airp <wenmin.pei@gmail.com>
2024-11-22 12:20:00 +0800

    add exclude_patterns

--- config/nvim/lua/autocmds.lua
+++ config/nvim/lua/autocmds.lua
@@ -48,14 +48,32 @@ usercmd("CloseUnmodifiedGitBuffers", function()
   local modified_buf = nil
   local to_delete = {}
 
+  local exclude_patterns = {
+    "%.pb%.go$",
+    "%.pb%.validate%.go$",
+  }
+
   for _, buf in ipairs(buffers) do
     if vim.api.nvim_buf_is_loaded(buf) and vim.bo[buf].modifiable and vim.fn.bufname(buf) ~= "" then
-      local ok, hunks = pcall(gitsigns.get_hunks, buf)
-      if ok and vim.tbl_isempty(hunks) then
+      local buf_name = vim.fn.bufname(buf)
+      local is_excluded = false
+      for _, pattern in ipairs(exclude_patterns) do
+        if buf_name:match(pattern) then
+          is_excluded = true
+          break
+        end
+      end
+
+      if is_excluded then
         table.insert(to_delete, buf)
       else
-        if buf == vim.api.nvim_get_current_buf() or modified_buf == nil then
-          modified_buf = buf
+        local ok, hunks = pcall(gitsigns.get_hunks, buf)
+        if ok and vim.tbl_isempty(hunks) then
+          table.insert(to_delete, buf)
+        else
+          if buf == vim.api.nvim_get_current_buf() or modified_buf == nil then
+            modified_buf = buf
+          end
         end
       end
     end
@@ -66,7 +84,7 @@ usercmd("CloseUnmodifiedGitBuffers", function()
   end
 
   for _, buf in ipairs(to_delete) do
-    -- print("Buffer ID: " .. buf .. " Buffer Name: " .. vim.fn.bufname(buf))
+    -- print("Delete Buffer ID: " .. buf .. " Buffer Name: " .. vim.fn.bufname(buf))
     vim.api.nvim_buf_delete(buf, { force = true })
   end
 end, {})
