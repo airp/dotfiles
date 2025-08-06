a1812a1d536f22b80beb826fcc53d9b161589ca6
airp <wenmin.pei@gmail.com>
2025-08-06 16:14:13 +0800

    update

--- config/nvim/lua/configs/nvimtree.lua
+++ config/nvim/lua/configs/nvimtree.lua
@@ -5,6 +5,13 @@ return function()
     local api = require "nvim-tree.api"
     api.config.mappings.default_on_attach(bufnr)
     vim.keymap.del("n", "<C-k>", { buffer = bufnr })
+    vim.keymap.del("n", "s", { buffer = bufnr })
+    vim.keymap.set("n", "t", api.node.run.system, {
+      desc = "Run System",
+      buffer = bufnr,
+      noremap = true,
+      silent = true,
+    })
   end
 
   opts.on_attach = my_on_attach
