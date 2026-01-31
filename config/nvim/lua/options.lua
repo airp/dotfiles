a8bc2c2ce921d2554a9b5138fe21ee6561c2ee22
airp <wenmin.pei@gmail.com>
2026-01-31 14:45:27 +0800

    update

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -13,3 +13,5 @@ g.wordmotion_nomap = true
 -- vim-tmux-navigator
 g.tmux_navigator_no_wrap = 1
 g.tmux_navigator_disable_when_zoomed = 1
+vim.opt.relativenumber = true
+-- vim.wo.relativenumber = true
